6b109acb41bffaa56279a6721b7d4d94bcc12ef5
Matus <matus.zilinsky@gmail.com>
2020-08-15 14:30:52 +0200

    redirect to https

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use (function (req, res, next) {
+    if (req.secure) {
+            next();
+    } else {
+            res.redirect('https://' + req.headers.host + req.url);
+    }
+});
+
 app.use(cors());
 app.use(express.json());
 
